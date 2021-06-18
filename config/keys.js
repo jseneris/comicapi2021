47f7588155f81ebf16dfc5a3a01486351c24004a
John Seneris <john@covetlyapp.com>
2021-06-18 16:45:39 -0400

    reverting changes

--- config/keys.js
+++ config/keys.js
@@ -4,5 +4,5 @@ if (process.env.NODE_ENV === 'production') {
   module.exports = require('./prod');
 } else {
   //dev keys
-  module.exports = require('./prod');
+  module.exports = require('./dev');
 }
