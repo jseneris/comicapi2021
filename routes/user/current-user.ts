e32786098548d138858a523237757dc2c1c43818
John Seneris <john@covetlyapp.com>
2021-10-18 11:04:52 -0400

    debugging

--- routes/user/current-user.ts
+++ routes/user/current-user.ts
@@ -5,14 +5,18 @@ import { User, UserDoc } from '../../models/User';
 const router = express.Router();
 
 router.get('/api/user/currentuser', currentUser, async (req, res) => {
+  console.log('currentUser', currentUser);
   const user = await User.findOne({
     auth0Id: req.currentUser?.sub,
   });
 
   let retval = null;
   if (user) {
+    console.log('user', user);
     const { userName, email } = user as UserDoc;
     retval = { userName, email };
+  } else {
+    console.log('no user');
   }
 
   res.send({ currentUser: retval });
